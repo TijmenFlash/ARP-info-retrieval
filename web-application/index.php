e9c723be71dae0334953ca4f5822044e6787aa71
Tijmen van Willigen <t.m.vanwilligen@student.utwente.nl>
2016-11-29 10:58:43 +0100

    updates
    updates

--- web-application/index.php
+++ web-application/index.php
@@ -1,9 +1,16 @@
+<!doctype html>
 <html>
 	<head>
+		<meta charset="utf-8">
   		<title>PHP Test</title>
   		<link rel="stylesheet" type="text/css" href="style.css">
  	</head>
  	<body>
- 		<p>Hello World</p>
+ 		<form action="index.php" method="get" autocomplete="off">
+ 			<label>
+ 				Search for something
+ 				<input type="text" name="q">
+ 			</label>
+ 		</form>
  	</body>
 </html>
